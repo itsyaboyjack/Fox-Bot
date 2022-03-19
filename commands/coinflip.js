71e2e25581ca0253139993e79f335f1d365f15b6
itsyaboyjack <72960618+itsyaboyjack@users.noreply.github.com>
2022-03-18 21:01:09 -0400

    Delete commands directory

--- commands/coinflip.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
-      var coinflip = [':coin: Heads!',':coin: Tails!'];
-      message.channel.send(coinflip[Math.floor(Math.random () * coinflip.length)]);
-}
